05b7f559123c874ab210173bc00c43d2932f8e3d
shradhakatyal <skatyal2204@gmail.com>
2018-11-11 19:18:53 +0530

    post endpoint added

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -18,6 +20,11 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  var outputObject = {name: req.file.originalname, type: req.file.mimetype, size: req.file.size};
+  res.send(outputObject);
+});
+
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+  console.log('Server is running on - '+(process.env.PORT || 3000));
 });
